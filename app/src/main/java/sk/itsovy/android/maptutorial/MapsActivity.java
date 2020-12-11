9830268c6bf31b7f192dc04d71d7b9e203f5839a
miroslav-opiela <miroslav-opiela@upjs.sk>
2020-12-11 11:07:43 +0100

    permissions

--- app/src/main/java/sk/itsovy/android/maptutorial/MapsActivity.java
+++ app/src/main/java/sk/itsovy/android/maptutorial/MapsActivity.java
@@ -1,11 +1,17 @@
 package sk.itsovy.android.maptutorial;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 
@@ -20,12 +26,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap map;
     private FusedLocationProviderClient fusedLocationProviderClient;
     private Location location;
+
+    private static final int PERMISSION_CODE = 33;
+    private static final String PERM = Manifest.permission.ACCESS_FINE_LOCATION;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,25 +53,68 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             @Override
             public void onClick(View view) {
                 // kontrolovat povolenie
-                getLastLocation();
+                if (checkPermissions()) {
+                    getLastLocation();
+                } else {
+                    requestPermissions();
+                }
+
             }
         });
     }
 
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[]{PERM}, PERMISSION_CODE);
+    }
+
+    private boolean checkPermissions() {
+        return ActivityCompat.checkSelfPermission(this, PERM) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // ci ide o povolenie na location
+        if (requestCode == PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // ok mam povolenie
+                getLastLocation();
+            } else {
+                // nedostal som povolenie
+
+                Snackbar.make(findViewById(android.R.id.content), "permissions denied",
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Go to settings", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        })
+                        .show();
+
+            }
+        }
+    }
+
     @SuppressLint("MissingPermission")
     private void getLastLocation() {
-         Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
-         locationTask.addOnCompleteListener(this, new OnCompleteListener<Location>() {
-             @Override
-             public void onComplete(@NonNull Task<Location> task) {
-                 if (task.isSuccessful()) {
-                     location = task.getResult();
-                     Log.d("MAP", location.toString());
-                 } else {
-                     Log.e("MAP", "could not load location ", task.getException());
-                 }
-             }
-         });
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if (task.isSuccessful()) {
+                    location = task.getResult();
+                    Log.d("MAP", location.toString());
+                } else {
+                    Log.e("MAP", "could not load location ", task.getException());
+                }
+            }
+        });
     }
 
     /**
